99a93d59915d64ae1df15d7300b4574a5191bda7
stasi815 <anastasia.gallardo@students.makeschool.com>
2020-05-07 10:09:52 -0700

    Updates

---
+++ precache-manifest.a792146e43c176686c801c4f1abf7edc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42c70b2221404136b291e19a7a3816d3",
+    "url": "/Counter-with-React/index.html"
+  },
+  {
+    "revision": "2aa85901d8a751496bd8",
+    "url": "/Counter-with-React/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e4254d3b9a43b8e588b0",
+    "url": "/Counter-with-React/static/js/2.03b35ab4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Counter-with-React/static/js/2.03b35ab4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2aa85901d8a751496bd8",
+    "url": "/Counter-with-React/static/js/main.fcc43564.chunk.js"
+  },
+  {
+    "revision": "10e93b1960f262715f1a",
+    "url": "/Counter-with-React/static/js/runtime-main.8b43359e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Counter-with-React/static/media/logo.5d5d9eef.svg"
+  }
+]);
